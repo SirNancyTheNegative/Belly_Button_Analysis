129d15d6a06ab6c21c216d04ec3b9c9a28c29e6b
SirNancyTheNegative <77301673+SirNancyTheNegative@users.noreply.github.com>
2021-04-24 21:03:04 -0700

    Trying autotypenumbers false in 1st graph

--- charts.js
+++ charts.js
@@ -77,7 +77,7 @@ function buildCharts(sample) {
     // 8. Create the trace for the bar chart. 
     var barData = [{
       x: sample_values.slice(0,10).reverse(),
-      y: yticks,
+      y: otu_ids,
       text: otu_labels.slice(0,10),
       type:"bar",
       orientation: 'h'
